a8674ec0924b1f0c6caf69d1e1deced3c36b0de9
JavaScript Joe <jjprevite@gmail.com>
2019-11-11 20:35:50 -0700

    feat: refactor binary-search

--- binary-search/binary-search.test.ts
+++ binary-search/binary-search.test.ts
@@ -1,32 +1,39 @@
-import { indexOf } from '.';
+import { search } from '.';
 
-describe('indexOf function', () => {
+describe('search function', () => {
   it('returns -1 if the item does not exist in the array', () => {
     const target = 0;
     const items = [1, 2, 3];
-    const actual = indexOf<number>(target, items);
+    const actual = search<number>(items, target);
     const expected = -1;
     expect(actual).toBe(expected);
   });
   it('returns the index in a small group of items', () => {
     const target = 6;
     const items = [1, 3, 6];
-    const actual = indexOf<number>(target, items);
+    const actual = search<number>(items, target);
     const expected = 2;
     expect(actual).toBe(expected);
   });
   it('returns the index in a medium group of items', () => {
     const target = 6;
     const items = [1, 3, 6, 7, 9, 14, 18, 21];
-    const actual = indexOf<number>(target, items);
+    const actual = search<number>(items, target);
     const expected = 2;
     expect(actual).toBe(expected);
   });
   it('returns the index in a larger group of items', () => {
     const target = 9;
     const items = [1, 3, 6, 7, 9, 14, 18, 21];
-    const actual = indexOf<number>(target, items);
+    const actual = search<number>(items, target);
     const expected = 4;
+    expect(actual).toBe(expected);
+  });
+  it('returns the index in a group of two items', () => {
+    const target = 2;
+    const items = [2, 5];
+    const actual = search<number>(items, target);
+    const expected = 0;
     expect(actual).toBe(expected);
   });
 });
--- binary-search/index.ts
+++ binary-search/index.ts
@@ -1,33 +1,28 @@
 /**
+ * @param nums the list of nums in which you're looking
  * @param target the value you are looking for
- * @param items the list of items in which you're looking
  * @returns the index of the target item if it exists. Otherwise, returns -1
  */
-export function indexOf<TypeOfValue>(
-  target: TypeOfValue,
-  items: TypeOfValue[]
-) {
+export function search<TypeOfValue>(nums: TypeOfValue[], target: TypeOfValue) {
   // left most value (index of value)
   let min = 0;
   // right most value (index of value)
-  let max = items.length - 1;
+  let max = nums.length - 1;
   let currentIndex;
-  let count = items.length;
 
-  while (count > 0) {
+  while (max >= min) {
     currentIndex = Math.round((min + max) / 2);
-    const currentValue = items[currentIndex];
+    const currentValue = nums[currentIndex];
     // Check if we found the target
     if (currentValue === target) {
       return currentIndex;
     } else if (currentValue < target) {
       // shift right
-      min = currentIndex;
+      min = min + 1;
     } else {
       // shift left
-      max = currentIndex;
+      max = max - 1;
     }
-    count -= 1;
   }
   return -1;
 }
