04a10565c53acb40439f199456c3b8cf5869d194
JavaScript Joe <jjprevite@gmail.com>
2019-11-22 21:34:32 -0700

    feat: add twoSumFaster

--- src/twoSum/index.ts
+++ src/twoSum/index.ts
@@ -1,7 +1,4 @@
 export function twoSum(nums: number[], target: number) {
-  // Write the function body here
-  // We want to search the array for two numbers that add up to equal the target
-
   // If there are no numbers in nums, we return false
   if (nums.length < 2) return false;
 
@@ -25,3 +22,52 @@ export function twoSum(nums: number[], target: number) {
   Worst case scenario - O(n^2) because we have to loop through all the nums twice
   We have to check every permutation.
 */
+
+export function twoSumFaster(nums: number[], target: number) {
+  // If there are no numbers in nums, we return false
+  if (nums.length < 2) return false;
+
+  // Keep track of the numbers we have seen
+  // We use a Map, so that we don't have to coerce the numbers into strings to use as keys in the object
+  let seenNumbers = new Map();
+
+  // Don't store currentNumber (because the array contains them, we'd be duplicating them)
+  // store the difference between the target - currentNumber
+  // index makes it happen
+  // key: number, value: index
+
+  // [1, 2, 3]
+  // target = 4
+  // key = 4-1 => 0
+
+  /*
+  {
+    // if you see 3, the complement is located at index 0
+    3: 0
+  }
+  */
+
+  // Loop through nums
+  for (let i = 0; i < nums.length; i++) {
+    // The number that would complement the number to equal the target
+
+    // [1, 2, 3]
+    // i = 0
+    // nums[i] = 1
+    // complement = 3
+    // seenNumbers = { 3: 0 }
+
+    // Check if the complement is not in our seenNumbers
+    if (seenNumbers.has(nums[i])) {
+      // We found it! :100:
+      return [seenNumbers.get(nums[i]), i];
+    } else {
+      const complement = target - nums[i];
+      // Store it in our seenNumbers
+      seenNumbers.set(complement, i);
+    }
+  }
+  console.log(seenNumbers);
+
+  return false;
+}
--- src/twoSum/twoSum.test.ts
+++ src/twoSum/twoSum.test.ts
@@ -1,4 +1,4 @@
-import { twoSum } from '.';
+import { twoSum, twoSumFaster } from '.';
 
 describe('twoSum function', () => {
   it('should work with an array with 4 numbers', () => {
@@ -27,6 +27,37 @@ describe('twoSum function', () => {
     const target = 9;
     const expected = false;
     const actual = twoSum(nums, target);
+    expect(actual).toBe(expected);
+  });
+});
+
+describe('twoSumFaster function', () => {
+  it('should work with an array with 4 numbers', () => {
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    const expected = [0, 1];
+    const actual = twoSumFaster(nums, target);
+    expect(actual).toEqual(expected);
+  });
+  it('should not work with an empty array', () => {
+    const nums: number[] = [];
+    const target = 9;
+    const expected = false;
+    const actual = twoSumFaster(nums, target);
+    expect(actual).toBe(expected);
+  });
+  it('should work with an array with 3 numbers', () => {
+    const nums = [1, 2, 3];
+    const target = 4;
+    const expected = [0, 2];
+    const actual = twoSumFaster(nums, target);
+    expect(actual).toEqual(expected);
+  });
+  it('should not work with an array with 1 numbers', () => {
+    const nums = [0];
+    const target = 9;
+    const expected = false;
+    const actual = twoSumFaster(nums, target);
     expect(actual).toBe(expected);
   });
 });
